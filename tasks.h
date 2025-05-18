fd6754aedda638ddb91e1b18e8430244cf17e401
danila-sosnov <dsosnov2009@gmail.com>
2025-05-18 16:21:10 +0300

    Добавьте файлы проекта.

---
+++ tasks.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+bool task01(int, int, int, int);
+bool task02(int, int, int, int);
+bool task03(int, int, int, int);
+bool task04(int, int, int, int);
